e805feb149fb68820264ae8629a9868d86a9035b
Maras59 <marcosespino103@gmail.com>
2025-03-26 21:30:31 -0700

    Add tkinter selection scaffolding

--- NyaaClient.py
+++ NyaaClient.py
@@ -1,5 +1,35 @@
 import tkinter as tk
 
+# TODO: Precheck csv and set isempty
+isempty = True
+
+def on_select(event):
+    if listbox.curselection():
+      if isempty:
+          listbox.selection_clear(0)
+
+
 root = tk.Tk(screenName=None, baseName="NyaaClient", className="NyaaClient", useTk=1)
+
+listbox = tk.Listbox(root, height = 10,
+                  bg = "white",
+                  activestyle = 'dotbox', 
+                  font = "Helvetica",
+                  fg = "gray")
+
+root.geometry("250x300") 
+
+if isempty:
+    listbox.insert(0, "Nothing Here...")
+    listbox.itemconfig(0, {'fg': 'gray'})
+
+label = tk.Label(root, text = "SHOWS")
+
+label.pack(fill="x")
+
+# Bind the selection event
+listbox.bind("<<ListboxSelect>>", on_select)
+
+listbox.pack(fill="both", expand=True, padx=10, pady=5)
 
 root.mainloop()
